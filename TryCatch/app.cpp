726d540b2a1c752dab50055263644314dd9b45fb
olegsmvl <33567178+olegsmvl@users.noreply.github.com>
2021-09-08 00:58:45 +0300

    add try catch project

---
+++ TryCatch/app.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+ 
+double divide(int, int);
+ 
+int main()
+{
+    int x = 500;
+    int y = 0;
+    try
+    {
+        double z = divide(x, y);
+        std::cout << z << std::endl;
+    }
+    catch (const char* msg)
+    {
+        std::cout << msg << std::endl;
+    }
+    std::cout << "The End..." << std::endl;
+    return 0;
+}
+
+double divide(int a, int b)
+{
+    if (b == 0)
+        throw "Division by zero!";
+    return a / b;
+}
