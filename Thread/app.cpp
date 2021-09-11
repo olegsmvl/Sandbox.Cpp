e7b1e3d16f58ec6d95675340810db5cf4d4d728b
olegsmvl2@mail.ru <olegsmvl2@mail.ru>
2021-09-11 23:50:10 +0300

    add thread

--- Thread/app.cpp
+++ Thread/app.cpp
@@ -2,16 +2,26 @@
 #include <thread>
 
 void threadFunction(int value){
-    value++;
-    std::cout << value << std::endl;
+    for (size_t i = 0; i < 10; i++)
+    {
+        std::cout << i << " second thread" << std::endl;    
+        /* code */
+    }
 }
 
 int main(int argc, char const *argv[])
 {
     int a = 1;
     std::thread thr(threadFunction, a);
-    thr.join();
-    std::cout << a << std::endl;
+    //thr.join();
+    thr.detach();
+
+    for (size_t i = 0; i < 10; i++)
+    {
+        std::cout << i << std::endl;    
+        /* code */
+    }
+    
 
     /* code */
     return 0;
