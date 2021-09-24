2e8ad35461fd6e308a21ebcd66784f3fd6f75f5f
olegsmvl <33567178+olegsmvl@users.noreply.github.com>
2021-09-24 20:00:34 +0300

    add string

---
+++ String/app.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    string str1 = "start_end";
+    string str2 = "start_ent";
+    cout << str1 << endl;
+    bool equal = str1 < str2 ;
+    cout << equal << endl;
+    cout << str1.size() << endl;
+    
+    char c = str1[1];
+    cout << c << endl;
+
+    str1[0] = 'M';
+    cout << str1 << endl;
+
+    char symbols[] = {'A', 'B', '\0'};
+    cout << symbols << endl;
+
+    auto res = (int)str1.find("art1");
+
+    cout << res << endl;
+
+    return 0;
+}
