53808cd434cca8fa9d19d1d8bf6766482833d8ae
olegsmvl2@mail.ru <olegsmvl2@mail.ru>
2021-09-26 01:43:26 +0300

    add smart pointer

--- SmartPointer/Unique/app.cpp
+++ SmartPointer/Unique/app.cpp
@@ -1,8 +1,35 @@
 #include <iostream>
+#include <memory>
+#include <string>
 using namespace std;
+
+class Item{
+    private:
+     string name;
+    
+    public:
+    Item(string str){
+        name = str;
+        cout << "ctor" << endl;
+        }
+    ~Item(){cout << "dtor" << endl;}
+    string getValue() {return name;}
+};
 
 int main(int argc, char const *argv[])
 {
+    //auto p = new Item();
+    unique_ptr<Item> item1(new Item("first"));
+    auto item2 = move(item1);
+
+    cout << item2->getValue() << endl;
+
+    if (item1 == nullptr)
+     cout << "null" << endl;
+
+    auto item3 = make_unique<Item>("third");
+
+    cout << item3->getValue() << endl;
 
     return 0;
 }
