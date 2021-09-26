461a61a4e8329d18eea9f8bd9767724a0dd132a4
olegsmvl <33567178+olegsmvl@users.noreply.github.com>
2021-09-26 15:31:57 +0300

    un ordered map

--- PracticJunior/Unordered_map/app.cpp
+++ PracticJunior/Unordered_map/app.cpp
@@ -1,8 +1,34 @@
 #include <iostream>
+#include <string>
+#include <unordered_map>
 using namespace std;
 
 int main(int argc, char const *argv[])
 {
+    auto hash_table = new unordered_map<string, int>{
+        {"first", 1},
+        {"second", 2},
+        {"third", 3},
+        {"four", 4},
+    };
+
+    auto pair = hash_table->find("second");
+
+    auto start = hash_table -> begin();
+    auto end = hash_table -> end();
+
+    for (const std::pair<string, int> x: hash_table){
+
+    }
+
+    if(pair == hash_table->end()){
+        cout << "not found" << endl;
+        return 0;
+    }
+
+    cout << pair->first << endl;
+    cout << pair->second << endl;
+    cout << "size: " << hash_table->size() << endl;
 
     return 0;
 }
--- PracticJunior/Unordered_map/build.bat
+++ PracticJunior/Unordered_map/build.bat
@@ -1,4 +1,4 @@
 rmdir /s /q build
 mkdir build
-g++ app.cpp -o build\app.exe
+g++ -std=c++17 app.cpp -o build\app.exe
 build\app.exe
