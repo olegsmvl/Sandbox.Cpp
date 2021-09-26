4bf8d042ea83281c1b44de1f572ee10f1f291aec
olegsmvl <33567178+olegsmvl@users.noreply.github.com>
2021-09-26 22:13:40 +0300

    unorderd map solved

--- PracticJunior/Unordered_map/app.cpp
+++ PracticJunior/Unordered_map/app.cpp
@@ -3,65 +3,57 @@
 #include <unordered_map>
 using namespace std;
 
-void find_test(unordered_map<string, int> *hash_table);
-void print(unordered_map<string, int> *hash_table);
+void find_test(unordered_map<string, int> &hash_table);
+void print(unordered_map<string, int> &hash_table);
 
 int main(int argc, char const *argv[])
 {
-    auto hash_table = new unordered_map<string, int>{
+    unordered_map<string, int> hash_table{
         {"first", 1},
         {"third", 3},
         {"second", 2},
         {"four", 4},
+        {"five", 5},
+        {"six", 6},
     };
 
-
-    unordered_map<string, int>::iterator iter = hash_table -> begin();
-    unordered_map<string, int>::iterator end = hash_table -> end();
-
-    // for (unordered_map<string, int>::iterator p = hash_table ->begin(); p!= hash_table->end(); p++){
-    //     if (p->second % 2 == 0){
-    //         cout << p->first << endl;
-    //         hash_table->erase(p);
-    //     }
-    // }
-
-    while (iter != hash_table -> end())
+    for (auto p = hash_table.begin(); p != hash_table.end(); )
     {
-        if (iter->second % 2 == 0){
-            cout << iter->first << endl;
-            hash_table->erase(iter);
+        if (p->second % 2 == 0)
+        {
+            cout << p->first << endl;
+            p = hash_table.erase(p);
         }
+        else
+            ++p;
     }
-    
 
     cout << "=============" << endl;
 
     print(hash_table);
 
-    delete hash_table;
-
     return 0;
 }
 
-void print(unordered_map<string, int> *hash_table){
-    for (unordered_map<string, int>::iterator p = hash_table ->begin(); p!= hash_table->end(); p++){
-        if (p->second % 2 == 0){
-            cout << p->first << " " << p->second << endl;
-            hash_table->erase(p);
-        }
+void print(unordered_map<string, int> &hash_table)
+{
+    for (auto p = hash_table.begin(); p != hash_table.end(); p++)
+    {
+        cout << p->first << " " << p->second << endl;
     }
 }
 
-void find_test(unordered_map<string, int> *hash_table){
-    auto pair = hash_table->find("second");
+void find_test(unordered_map<string, int> &hash_table)
+{
+    auto pair = hash_table.find("second");
 
-    if(pair == hash_table->end()){
+    if (pair == hash_table.end())
+    {
         cout << "not found" << endl;
         return;
     }
 
     cout << pair->first << endl;
     cout << pair->second << endl;
-    cout << "size: " << hash_table->size() << endl;
+    cout << "size: " << hash_table.size() << endl;
 }
