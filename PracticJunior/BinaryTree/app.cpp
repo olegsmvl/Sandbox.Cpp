66c04b95c3e40b8adab9677ae6a0a3409f72efa1
olegsmvl <33567178+olegsmvl@users.noreply.github.com>
2021-09-29 14:40:46 +0300

    start binary tree

---
+++ PracticJunior/BinaryTree/app.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Node{
+    Node* left;
+    Node* right;
+};
+
+
+
+int main(int argc, char const *argv[])
+{
+
+
+    return 0;
+}
