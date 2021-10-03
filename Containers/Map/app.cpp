1c86d2d5b79fde9d32cc0a2c229d4eec3f2cd365
olegsmvl <33567178+olegsmvl@users.noreply.github.com>
2021-10-04 00:35:37 +0300

    map insert, find

--- Containers/Map/app.cpp
+++ Containers/Map/app.cpp
@@ -5,16 +5,32 @@ using namespace std;
 
 int main(int argc, char const *argv[])
 {
-    auto m = new map<string, int> {
+    auto m = new map<string, int>{
         {"Ann", 37},
         {"User", 27},
         {"Anton", 24},
-        {"Victor", 33}
-    };
+        {"Victor", 33}};
+
+    (*m)["Elena"] = 34;
+    m->insert({"Max", 45});
+    m->insert(make_pair("Vlad", 41));
 
     //auto iter = m->begin();
 
     for (auto iter = m->begin(); iter != m->end(); ++iter)
+    {
+        cout << iter->first << '\t';
+        cout << iter->second << endl;
+    }
+
+    cout << "===========" << endl;
+
+    auto iter = m->find("Max");
+    if (iter == m->end())
+    {
+        cout << "not found" << endl;
+    }
+    else
     {
         cout << iter->first << '\t';
         cout << iter->second << endl;
