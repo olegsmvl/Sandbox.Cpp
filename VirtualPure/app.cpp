5e46fe2e0e3b22f855325a55c6d7dbc4e5e04a5d
olegsmvl <33567178+olegsmvl@users.noreply.github.com>
2021-10-03 10:41:19 +0300

    pure virtual  call

--- VirtualPure/app.cpp
+++ VirtualPure/app.cpp
@@ -1,8 +1,32 @@
 #include <iostream>
 using namespace std;
 
+class A
+{
+    public:
+    A(){
+        cout << "ctor A" << endl;
+        init();
+    }
+    virtual void print() = 0;
+    private:
+    void init(){
+        print();
+    }
+};
+
+class B : public A{
+    public:
+    void print() {
+        cout << "print method" << endl;
+    }
+};
+
 int main(int argc, char const *argv[])
 {
+    //A cl;
+    B* cl = new B();
+    cl->print();
 
     return 0;
 }
