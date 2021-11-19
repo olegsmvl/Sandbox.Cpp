31b5f5e5ee5ea1c6c99d4a1e0b328e400143ed02
olegsmvl2@mail.ru <olegsmvl2@mail.ru>
2021-11-19 17:52:22 +0300

    solve 9-th task leetcode

--- LeetCode/9_easy/app.cpp
+++ LeetCode/9_easy/app.cpp
@@ -7,47 +7,32 @@ class Solution
 public:
     bool isPalindrome(int x)
     {
-        cout << "number: " << x << endl;
-        int digit = x % 10;
-        x = x / 10;
         vector<int> digits;
-        digits.push_back(digit);
 
-        while (x >= 10)
+        do
         {
-            digit = x % 10;
+            int digit = x % 10;
             digits.push_back(digit);
             x = x / 10;
-        }
-
-        cout << "digits: " ;
-
-        for (int dig : digits)
-            cout << dig << " ";
+        }while (x > 0);
 
         for (int i = 0; i < digits.size() / 2; i++)
-        {
-            cout << digits[i] << " ";
-            if (digits[i] != digits[digits.size() - i]){
-                cout << endl;
+            if (digits[i] != digits[digits.size() - 1 - i])
                 return false;
-            }
-        }
 
-        cout << endl;
         return true;
     }
 };
 
 int main(int argc, char const *argv[])
 {
-    vector<int> test_values{12345, 10, 15, 101, 222};
+    vector<int> test_values{12345, 1111, 111, 10, 55555, 123, 1234, 101};
     Solution sol;
 
     for (int x : test_values)
     {
         bool result = sol.isPalindrome(x);
-        cout << x << " - "<< result << endl;
+        cout << x << " - " << "\t" << result << endl;
     }
     return 0;
 }
---
+++ Temp/tmp/app.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    int num = 5;
+    int result = num / 10;
+    cout << result << endl;
+    return 0;
+}
