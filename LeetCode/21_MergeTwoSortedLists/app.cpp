1dee53f0f5e2cfae8893e1b8402a6e35417418c0
olegsmvl2@mail.ru <olegsmvl2@mail.ru>
2021-12-02 00:41:04 +0300

    solution in progress

--- LeetCode/21_MergeTwoSortedLists/app.cpp
+++ LeetCode/21_MergeTwoSortedLists/app.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <vector>
 using namespace std;
 
 struct ListNode
@@ -10,16 +11,64 @@ struct ListNode
     ListNode(int x, ListNode *next) : val(x), next(next) {}
 };
 
-class Solution {
+class Solution
+{
 public:
-    ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
+    ListNode *mergeTwoLists(ListNode *list1, ListNode *list2)
+    {
+        ListNode* first = nullptr;
         
+        ListNode* iter1 = list1;
+        ListNode* iter2 = list2;
+
+        if(iter1->val <= iter2->val)
+            first = iter1;
+
+        return list1;
     }
 };
 
+ListNode *insert(ListNode *node, int value)
+{
+    if (node == nullptr)
+        return new ListNode(value);
+
+    return new ListNode(value, node);
+}
+
+ListNode *createList(vector<int> &vec)
+{
+    ListNode *first = nullptr;
+    auto iter = vec.end();
+    while (iter != vec.begin())
+    {
+        iter--;
+        first = insert(first, *iter);
+    }
+    return first;
+}
+
+void print_list(ListNode* list){
+    ListNode* node = list;
+    
+    while (node != nullptr)
+    {
+        cout << node->val << " ";
+        node = node->next;
+    }
+    cout << endl;
+}
+
 int main(int argc, char const *argv[])
 {
-    int arr_list1[1,2,4]; 
-    int arr_list2[1,3,4]; 
+    vector<int> vec_list1{1, 2, 4};
+    vector<int> vec_list2{1, 3, 4};
+    ListNode *list1 = createList(vec_list1);
+    ListNode *list2 = createList(vec_list2);
+    Solution sol;
+    sol.mergeTwoLists(list1, list2);
+
+    print_list(list1);
+    print_list(list2);
     return 0;
 }
