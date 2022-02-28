7b2e41edc7260bda89dba1295fc805104e448e6e
olegsmvl <olegsmvl2@mail.ru>
2022-02-28 18:54:09 +0300

    add tasks

---
+++ Alghorithm/Task1/app.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+
+// class Solution{
+//     public:
+//      vector<int> solve(vector<int> a, vector<int> b);
+
+// };
+
+
+int main(){
+    cout << "hello";
+    return 0;
+}
