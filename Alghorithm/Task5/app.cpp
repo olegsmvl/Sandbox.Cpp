71ccf595bac1426a5a29776f090ed69e13f8b5ea
olegsmvl2@mail.ru <olegsmvl2@mail.ru>
2022-03-01 13:34:59 +0300

    add Intersection, Union

--- Alghorithm/Task5/app.cpp
+++ Alghorithm/Task5/app.cpp
@@ -1,21 +1,25 @@
 #include <iostream>
 #include <vector>
-#include <map>
+#include <unordered_map>
 using namespace std;
 
 class Solution
 {
 public:
-    int Solve(vector<vector<int>> &guest)
+    int Solve(vector<vector<int>> &guests)
     {
         int res = 0;
-        map<int, int> arriving;
-        map<int, int> leaving;
+        unordered_map<int, int> arriving;
+        unordered_map<int, int> leaving;
 
-        for (int i =0 ; i < guest.size(); i++){
-            arriving[guest[0]] += 1
-            leaving[guest[0]] += 1
+        for (auto guest : guests){
+            arriving[guest[0]] += 1;
+            leaving[guest[1]] += 1;
         }
+
+        int current = 0;
+
+        
 
         return res;
     }
