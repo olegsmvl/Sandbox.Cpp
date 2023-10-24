457351d88008dca8594c5a5d43fe585b29157b5a
olegsmvl <olegsmvl2@mail.ru>
2023-10-24 23:59:29 +0300

    graph dfs

---
+++ Alghorithm/LeetCode/695_Max Area of Island_graph/app.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+  int maxAreaOfIsland(vector<vector<int>> &grid) { return 0;}
+};
+
+int main() {
+  vector<vector<int>> grid = {{0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0},
+                              {0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0},
+                              {0, 1, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0},
+                              {0, 1, 0, 0, 1, 1, 0, 0, 1, 0, 1, 0, 0},
+                              {0, 1, 0, 0, 1, 1, 0, 0, 1, 1, 1, 0, 0},
+                              {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0},
+                              {0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0},
+                              {0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0}};
+  
+  Solution sol;
+  auto result = sol.maxAreaOfIsland(grid);
+
+  cout << "result: " << result << endl;
+  
+  return 0;
+}
