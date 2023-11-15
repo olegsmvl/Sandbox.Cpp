f8adecf0fc55864a7cda462884465b686363f5aa
olegsmvl <olegsmvl2@mail.ru>
2023-11-15 20:07:16 +0100

    vector pop_back

--- Containers/Vector/Simple/app.cpp
+++ Containers/Vector/Simple/app.cpp
@@ -1,37 +1,57 @@
+#include <algorithm>
 #include <iostream>
 #include <vector>
-#include <algorithm>
 using std::cout;
 using std::endl;
 using std::vector;
 using namespace std;
 
-void find_func(vector<int> vec);
+void find_func(vector<int> &vec);
+void remove_last(vector<int> &vec);
 
-int main(){
-	vector<int> numbers = {1,3,5,8};
-	//	cout << numbers[0] << endl;
+int main() {
+  vector<int> numbers = {1, 3, 5, 8};
+  //	cout << numbers[0] << endl;
 
-	for (int n : numbers)
-				cout << n << endl;
+  for (int n : numbers)
+    cout << n << endl;
 
-	numbers.push_back(1000);
+  numbers.push_back(1000);
 
-	cout << numbers.front() << endl;
-	cout << numbers.back() << endl;
+  cout << numbers.front() << endl;
+  cout << numbers.back() << endl;
+  cout << "-----------------" << endl;
+  find_func(numbers);
 
-	find_func(numbers);
 
-	return 0;
+
+  remove_last(numbers);
+
+  return 0;
 }
 
-void find_func(vector<int> vec){
-	cout << "begin" << endl;
-	int val = 5;
+void remove_last(vector<int> &vec) {
+  cout << "---full-----------------" << endl;
 
-	auto pos = find(vec.begin(), vec.end(), val);
+  for (int n : vec) {
+    cout << n << endl;
+  }
 
-	cout << *pos << endl;
-	cout << "end" << endl;
+  cout << "---remove last-----------------" << endl;
+
+  vec.pop_back();
+
+  for (int n : vec) {
+    cout << n << endl;
+  }
 }
 
+void find_func(vector<int> &vec) {
+  cout << "begin" << endl;
+  int val = 5;
+
+  auto pos = find(vec.begin(), vec.end(), val);
+
+  cout << *pos << endl;
+  cout << "end" << endl;
+}
