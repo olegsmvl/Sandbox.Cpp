4a5ddb8ae9f4bc8bdabb997b9e02ebe0415357c5
Oleg Smyslov <oleg.smyslov@evocargo.com>
2024-07-04 22:49:36 +0200

    c printf

---
+++ C_code/print/hello.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main() {
+    int number = 42;  // Declare and initialize an integer variable
+    printf("Hello, World!\n");
+    printf("The value of the variable is: %d\n", number);  // Print the value of the variable
+    return 0;
+}
