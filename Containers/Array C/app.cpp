8076709c2fc0c7539e91f4ddebd02cb3bd404f9a
olegsmvl <olegsmvl2@gmail.com>
2024-07-18 14:30:55 +0200

    array iterator

--- Containers/Array C/app.cpp
+++ Containers/Array C/app.cpp
@@ -8,6 +8,11 @@ int main(int argc, char const *argv[])
     int arr[]{1, 3, 4, 5};
     cout << "size of arr: " << sizeof(arr) << endl;
     print(arr, 4);
+
+    int arr2[4];
+    arr2 = {1,2,3,4}
+    print(arr2, 8);
+
     return 0;
 }
 
--- Template/template_specialization/app.cpp
+++ Template/template_specialization/app.cpp
@@ -1,21 +1,33 @@
 #include <iostream>
 using namespace std;
 
-template<typename T>
-struct MyClass{
-    T value;
+template <typename T> struct MyClass {
+  MyClass() {}
+  MyClass(T val) : value(val) { cout << "default" << endl; }
+  T value;
 };
 
-template<>
-struct MyClass<int>{
-    int value = 6;
+template <> struct MyClass<int> {
+  MyClass() {}
+  MyClass(int val) : value(val) { cout << "specialization" << endl; }
+  int value = 6;
 };
 
-int main(int argc, char const *argv[])
-{
-    MyClass<int> t;
-    // t.value = 5;
+int main(int argc, char const *argv[]) {
+  MyClass<int> t;
+  // t.value = 5;
 
-    cout << t.value << endl;
-    return 0;
+  cout << t.value << endl;
+
+  float val = 7.5;
+  /* work since c++ 17 */
+  MyClass cl(val);
+  cout << cl.value << endl;
+
+  int val2 = 10;
+  /* work since c++ 17 */
+  MyClass cl2(val2);
+  cout << cl2.value << endl;
+
+  return 0;
 }
