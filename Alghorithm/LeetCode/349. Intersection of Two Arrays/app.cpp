57768dbce9ca4e476b0a20de2b9c581882a53945
olegsmvl <olegsmvl2@mail.ru>
2024-08-10 16:39:50 +0300

    update intersection task

--- Alghorithm/LeetCode/349. Intersection of Two Arrays/app.cpp
+++ Alghorithm/LeetCode/349. Intersection of Two Arrays/app.cpp
@@ -1,6 +1,7 @@
+#include <algorithm>
 #include <iostream>
 #include <vector>
-#include <algorithm>
+
 using namespace std;
 
 class Solution {
@@ -8,29 +9,26 @@ public:
   vector<int> intersection(vector<int> &nums1, vector<int> &nums2) {
     sort(nums1.begin(), nums1.end());
     sort(nums2.begin(), nums2.end());
+
     int i = 0;
     int j = 0;
-
+    int len_i = nums1.size();
+    int len_j = nums2.size();
     vector<int> result;
 
-    while ((i < nums1.size()) && (j < nums2.size())) {
-      if (nums1[i] < nums2[j]) {
+    while ((i < len_i) && (j < len_j)) {
+      if (nums1[i] == nums2[j]) {
+        if (result.size() == 0 || result[result.size() - 1] != nums1[i]) {
+          result.push_back(nums1[i]);
+        }
         i++;
+        j++;
       } else if (nums1[i] > nums2[j]) {
         j++;
-      } else {
-        int num = nums1[i];
-        result.push_back(num);
+      } else if (nums1[i] < nums2[j]) {
         i++;
-        j++;
-        while ((i < nums1.size()) && (j < nums2.size()) &&
-               (nums1[i] == nums2[j]) && (nums1[i] == num)) {
-          i++;
-          j++;
-        }
       }
     }
-
     return result;
   }
 };
